3e1fa8acc87a062df40029d3098169a3a6a2e664
stammath3 <stam.math.12345@gmail.com>
2024-09-02 17:15:50 +0300

    Initial commit.

---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../task.service';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [CardComponent, DatePipe],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+
+  constructor(private taskService: TasksService) {}
+
+  onCompleteTask() {
+    this.taskService.removeTask(this.task.id);
+  }
+}
